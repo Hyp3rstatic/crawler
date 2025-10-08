4c7d72722f99cc460909860491164b1e74d5fdee
Hyp3rstatic <kazjrosinski@gmail.com>
2025-10-07 22:38:07 -0700

    now keeps reference counts for sites

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,15 @@
 use reqwest::Client;
 use regex::Regex;
+use std::collections::HashMap;
 
-pub async fn getlinks(urltocrawl: &str, webqueue: Vec<String>) -> Result<Vec<String>, Box<dyn std::error::Error>> {
+pub async fn getlinks(urltocrawl: &str, webqueue: HashMap<String, i32>) -> Result<HashMap<String, i32>, Box<dyn std::error::Error>> {
     let mut webclient = Client::new();
     
     let mut webrequest = webclient.get(urltocrawl).send().await?;
 
     let relink = Regex::new("<a[^>]+href=\"(.*?)\"[^>]*>.*?</a>").unwrap();
 
-    let mut urllist: Vec<String> = vec![];
+    let mut urllist: HashMap<String, i32> = HashMap::new();
     urllist = webqueue.clone();
 
     if webrequest.status().is_success() {
@@ -27,8 +28,13 @@ pub async fn getlinks(urltocrawl: &str, webqueue: Vec<String>) -> Result<Vec<Str
             for link in rehttp.find_iter(&href) {
                 let url = &link.as_str()[1.. link.as_str().len()-1];
                 //println!("{}", text);
-                if !urllist.contains(&url.to_string()) {
-                    urllist.push(url.to_string());
+                if !urllist.contains_key(&url.to_string()) {
+                    urllist.insert(url.to_string(), 1);
+                }
+                else {
+                    let mut refstolink: i32 = *urllist.get(url).unwrap();
+                    refstolink += 1;
+                    urllist.insert(url.to_string(), refstolink);
                 }
             }
             
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,31 @@
 use crawler::getlinks;
+use std::collections::HashMap;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>>{
 
-    let mut webqueue: Vec<String> = vec![];
+    let mut webqueue: HashMap<String, i32> = HashMap::new();
 
     let mut seed = "https://www.rust-lang.org";
 
-    webqueue.push(seed.to_string());
+    webqueue.insert(seed.to_string(), 0);
 
     webqueue = getlinks(seed, webqueue).await?;
 
-    for i in 0..10 {
-        webqueue = getlinks(&webqueue.clone()[i+1], webqueue).await?;
+    for i in 0..1 {
+        let mut currentlink: String = "".to_string();
+        for (key, value) in webqueue.iter().take(i+1) {
+            //println!("{}: {}", key, value);
+            currentlink = key.clone();
+        }
+        webqueue = getlinks(currentlink.as_str(), webqueue).await?;
     }
 
     //println!("{:?}", webqueue);
     for link in &webqueue {
-        println!("{}", link);
+        if *link.1 > 1 {
+            println!("{:?}", link);
+        }
     }
 
     Ok(())
